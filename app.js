9c91bb0a1f7cb1b0ff9832e16a40779d88e3c335
mgravemeyer <m.gravemeyer@icloud.com>
2021-08-26 13:20:35 +0000

    added placeholder method

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = Vue.createApp({
 			gender: 'male',
 			picture: 'https://randomuser.me/api/portraits/men/10.jpg'
 		}
+	},
+	methods:  {
+		getUser() {
+			console.log(this.firstName)
+		}
 	}
 })
 
